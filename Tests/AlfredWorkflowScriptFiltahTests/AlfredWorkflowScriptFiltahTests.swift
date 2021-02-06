a317d0deaa7fe8aecb7f1482fcca0668281e66a9
Guill Lo <guill.bout@gmail.com>
2021-02-07 02:28:26 +0800

    fix some stuff reported by SwiftLint

--- Tests/AlfredWorkflowScriptFiltahTests/AlfredWorkflowScriptFiltahTests.swift
+++ Tests/AlfredWorkflowScriptFiltahTests/AlfredWorkflowScriptFiltahTests.swift
@@ -4,7 +4,10 @@ import XCTest
 final class AlfredWorkflowScriptFiltahTests: XCTestCase {
     func test_that_the_output_may_contain_zero_item() {
         let output = ["items": []]
-        let string = String(data: try! JSONSerialization.data(withJSONObject: output, options: []), encoding: String.Encoding.utf8)
+        let string = String(
+            data: try! JSONSerialization.data(withJSONObject: output, options: []),
+            encoding: String.Encoding.utf8
+        )
 
         XCTAssertEqual(
             ScriptFilter.output(),
