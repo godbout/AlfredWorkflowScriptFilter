f842eca6ef60b8cd9bc581e75d75b9795febad97
Guill Lo <guill.bout@gmail.com>
2021-02-09 18:37:13 +0800

    refactor tests

--- Tests/AlfredWorkflowScriptFiltahTests/ScriptFilterTests.swift
+++ Tests/AlfredWorkflowScriptFiltahTests/ScriptFilterTests.swift
@@ -2,10 +2,11 @@ import XCTest
 @testable import AlfredWorkflowScriptFiltah
 
 final class AlfredWorkflowScriptFiltahTests: XCTestCase {
+    private let scriptFilter = ScriptFilter.shared
 
-    func test_that_it_can_return_an_output_where_nothing_has_been_added() {
+    func test_that_it_may_contain_nothing() {
         let expectedOutput = #"{"items":[]}"#
-        let output = ScriptFilter.shared.output()
+        let output = scriptFilter.output()
 
         XCTAssertEqual(
             expectedOutput,
@@ -13,18 +14,18 @@ final class AlfredWorkflowScriptFiltahTests: XCTestCase {
         )
     }
 
-    func test_that_it_can_return_an_output_with_a_rerun() {
+    func test_that_it_may_contain_a_rerun() {
         XCTAssertEqual(
-            ScriptFilter.shared.rerun(secondsToWait: 0.0).output(),
+            scriptFilter.rerun(secondsToWait: 0.0).output(),
             #"{"items":[]}"#
         )
 
         XCTAssertEqual(
-            ScriptFilter.shared.rerun(secondsToWait: 5.1).output(),
+            scriptFilter.rerun(secondsToWait: 5.1).output(),
             #"{"items":[]}"#
         )
 
-        let output = ScriptFilter.shared.rerun(secondsToWait: 1.3).output()
+        let output = scriptFilter.rerun(secondsToWait: 1.3).output()
         let expectedOutput = #"{"rerun":1.3,"items":[]}"#
 
         XCTAssertEqual(
@@ -32,5 +33,4 @@ final class AlfredWorkflowScriptFiltahTests: XCTestCase {
             try JSONSerialization.jsonObject(with: Data(expectedOutput.utf8), options: []) as! NSDictionary
         )
     }
-
 }
