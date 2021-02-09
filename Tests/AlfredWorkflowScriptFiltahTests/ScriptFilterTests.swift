d96ece95c8d1572d351f34d4ed7e0d28168ff8cc
Guill Lo <guill.bout@gmail.com>
2021-02-09 18:10:49 +0800

    add some more tests for rerun possible range

--- Tests/AlfredWorkflowScriptFiltahTests/ScriptFilterTests.swift
+++ Tests/AlfredWorkflowScriptFiltahTests/ScriptFilterTests.swift
@@ -14,14 +14,22 @@ final class AlfredWorkflowScriptFiltahTests: XCTestCase {
     }
 
     func test_that_it_can_return_an_output_with_a_rerun() {
-        let expectedOutput = #"{"rerun":1.3,"items":[]}"#
-        let output = ScriptFilter.shared
-            .rerun(secondsToWait: 1.3)
-            .output()
+        XCTAssertEqual(
+            ScriptFilter.shared.rerun(secondsToWait: 0.0).output(),
+            #"{"items":[]}"#
+        )
 
         XCTAssertEqual(
-            try JSONSerialization.jsonObject(with: Data(expectedOutput.utf8), options: []) as! NSDictionary,
-            try JSONSerialization.jsonObject(with: Data(output.utf8), options: []) as! NSDictionary
+            ScriptFilter.shared.rerun(secondsToWait: 5.1).output(),
+            #"{"items":[]}"#
+        )
+
+        let output = ScriptFilter.shared.rerun(secondsToWait: 1.3).output()
+        let expectedOutput = #"{"rerun":1.3,"items":[]}"#
+
+        XCTAssertEqual(
+            try JSONSerialization.jsonObject(with: Data(output.utf8), options: []) as! NSDictionary,
+            try JSONSerialization.jsonObject(with: Data(expectedOutput.utf8), options: []) as! NSDictionary
         )
     }
 
