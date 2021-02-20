cb9e465a573940e166b5826d3df6c29d10bf11fb
Guill Lo <guill.bout@gmail.com>
2021-02-21 01:21:47 +0800

    add required fields for Item struct

--- Sources/AlfredWorkflowScriptFilter/Item.swift
+++ Sources/AlfredWorkflowScriptFilter/Item.swift
@@ -1,18 +1,28 @@
 //
 //  Item.swift
-//  AlfredWorkflowScriptFiltah
+//  AlfredWorkflowScriptFilter
 //
 //  Created by Guillaume Leclerc on 13/02/2021.
 //
 
 import Foundation
 
-struct Item {}
+struct Item {
+    let title: String
+    let subtitle: String
+    let icon: Icon
+
+    static func create(title: String, subtitle: String, icon: Icon) -> Item {
+        Item(title: title, subtitle: subtitle, icon: icon)
+    }
+}
 
 extension Item: Codable {}
 
 extension Item: Equatable {
-    static func == (_: Item, _: Item) -> Bool {
-        true
+    static func == (lhs: Item, rhs: Item) -> Bool {
+        lhs.title == rhs.title
+            && lhs.subtitle == rhs.subtitle
+            && lhs.icon == rhs.icon
     }
 }
