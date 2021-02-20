ce334cfbb0059bd0bfa4bea817c72cce385fed5d
Guill Lo <guill.bout@gmail.com>
2021-02-21 01:32:45 +0800

    empty ItemTests for now
    gonna need to add some robust shit

--- Tests/AlfredWorkflowScriptFilterTests/ItemTests.swift
+++ Tests/AlfredWorkflowScriptFilterTests/ItemTests.swift
@@ -1,8 +1,4 @@
 @testable import AlfredWorkflowScriptFilter
 import XCTest
 
-class ItemTests: XCTestCase {
-    func test_that_it_requires_at_least_a_title_a_subtitle_and_an_icon() {
-        let item = Item.create(title: "title", subtitle: "subtitle", icon: Icon())
-    }
-}
+class ItemTests: XCTestCase {}
