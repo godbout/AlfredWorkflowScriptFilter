762fc46624510e4fdd55ca8a9a75153a8ba05a16
Guill Lo <guill.bout@gmail.com>
2021-03-01 20:02:09 +0800

    add subtitle, arg, valid and icon for Mods

--- Sources/AlfredWorkflowScriptFilter/Mod.swift
+++ Sources/AlfredWorkflowScriptFilter/Mod.swift
@@ -5,10 +5,31 @@
 class Mod: Codable {
     private var subtitle: String?
     private var arg: String?
+    private var valid: Bool?
+    private var icon: Icon?
+    private var variables: [String: String]?
 
-    init(subtitle: String? = nil, arg: String? = nil) {
+    init(subtitle: String? = nil, arg: String? = nil, valid: ItemValidity? = nil, icon: Icon? = nil) {
         self.subtitle = subtitle
         self.arg = arg
+
+        switch valid {
+        case .true:
+            self.valid = true
+        case .false:
+            self.valid = false
+        default:
+            self.valid = nil
+        }
+
+        self.icon = icon
+    }
+
+    @discardableResult
+    func icon(_ icon: Icon) -> Mod {
+        self.icon = icon
+
+        return self
     }
 }
 
@@ -16,8 +37,13 @@ extension Mod: Equatable {
     static func == (lhs: Mod, rhs: Mod) -> Bool {
         lhs.subtitle == rhs.subtitle
             && lhs.arg == rhs.arg
+            && lhs.valid == rhs.valid
+            && lhs.icon == rhs.icon
     }
 }
 
 class Ctrl: Mod {}
 class Cmd: Mod {}
+class Alt: Mod {}
+class Fn: Mod {}
+class Shift: Mod {}
