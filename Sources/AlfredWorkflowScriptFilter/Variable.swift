dfedecbe6332c68b516f9ae1c3800add72db7fa7
Guill Lo <guill.bout@gmail.com>
2021-03-01 21:39:37 +0800

    refactor Variable, remove create static fund

--- Sources/AlfredWorkflowScriptFilter/Variable.swift
+++ Sources/AlfredWorkflowScriptFilter/Variable.swift
@@ -1,8 +1,4 @@
 struct Variable {
     var name: String?
     var value: String?
-
-    static func create(name: String? = nil, value: String? = nil) -> Variable {
-        Variable(name: name, value: value)
-    }
 }
