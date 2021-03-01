17a37930df149f6e5e84c5b219953c8451812bc0
Guill Lo <guill.bout@gmail.com>
2021-03-01 21:43:52 +0800

    make Variable Codable and Equatable

--- Sources/AlfredWorkflowScriptFilter/Variable.swift
+++ Sources/AlfredWorkflowScriptFilter/Variable.swift
@@ -2,3 +2,12 @@ struct Variable {
     var name: String?
     var value: String?
 }
+
+extension Variable: Codable {}
+
+extension Variable: Equatable {
+    static func == (lhs: Variable, rhs: Variable) -> Bool {
+        lhs.name == rhs.name
+            && lhs.value == rhs.value
+    }
+}
