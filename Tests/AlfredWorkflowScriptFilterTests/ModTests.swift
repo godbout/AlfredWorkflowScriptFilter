bb5dafdb60dbb2e65714daa5e474af2be0d80e8e
Guill Lo <guill.bout@gmail.com>
2021-03-01 20:02:09 +0800

    add subtitle, arg, valid and icon for Mods

--- Tests/AlfredWorkflowScriptFilterTests/ModTests.swift
+++ Tests/AlfredWorkflowScriptFilterTests/ModTests.swift
@@ -31,4 +31,70 @@ class ModTests: XCTestCase {
             try JSONHelper().modObject(from: expectedOutput)
         )
     }
+
+    func test_that_it_may_be_valid() throws {
+        let mod = Alt(valid: .true)
+
+        let expectedOutput = """
+        {
+            "valid": true
+        }
+        """
+
+        XCTAssertEqual(
+            mod,
+            try JSONHelper().modObject(from: expectedOutput)
+        )
+    }
+
+    func test_that_it_may_not_be_valid() throws {
+        let mod = Fn(valid: .false)
+
+        let expectedOutput = """
+        {
+            "valid": false
+        }
+        """
+
+        XCTAssertEqual(
+            mod,
+            try JSONHelper().modObject(from: expectedOutput)
+        )
+    }
+
+    func test_that_it_maybe_have_an_icon() throws {
+        let mod = Shift(icon: Icon.create(path: "~/Dev"))
+
+        let expectedOutput = """
+        {
+            "icon": {
+                "path": "~/Dev"
+            }
+        }
+        """
+
+        XCTAssertEqual(
+            mod,
+            try JSONHelper().modObject(from: expectedOutput)
+        )
+    }
+
+    func test_that_it_cannot_have_multiple_icons() throws {
+        let mod = Shift(icon: Icon.create(path: "i con"))
+        mod.icon(Icon.create(path: "another", type: .fileicon))
+
+        let expectedOutput = """
+        {
+            "icon": {
+                "path": "another",
+                "type": "fileicon"
+            }
+        }
+        """
+
+        XCTAssertEqual(
+            mod,
+            try JSONHelper().modObject(from: expectedOutput)
+        )
+    }
 }
