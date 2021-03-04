808530b02d1608312c0e45b2f7d61cd657562927
Guill Lo <guill.bout@gmail.com>
2021-03-05 01:11:26 +0800

    add fluent API for Variable

--- Sources/AlfredWorkflowScriptFilter/Variable.swift
+++ Sources/AlfredWorkflowScriptFilter/Variable.swift
@@ -1,6 +1,30 @@
-struct Variable {
+final class Variable {
     var name: String?
     var value: String?
+
+    init(name: String? = nil, value: String? = nil) {
+        self.name = name
+        self.value = value
+    }
+
+    func name(_ name: String) -> Variable {
+        self.name = name
+
+        return self
+    }
+
+    func value(_ value: String) -> Variable {
+        self.value = value
+
+        return self
+    }
 }
 
 extension Variable: Codable {}
+
+extension Variable: Equatable {
+    static func == (lhs: Variable, rhs: Variable) -> Bool {
+        lhs.name == rhs.name
+            && lhs.value == rhs.value
+    }
+}
--- Tests/AlfredWorkflowScriptFilterTests/FluentAPITests.swift
+++ Tests/AlfredWorkflowScriptFilterTests/FluentAPITests.swift
@@ -2,7 +2,7 @@
 import XCTest
 
 class FluentAPITests: XCTestCase {
-    func test_that_there_is_one_for_Icons() {
+    func test_that_there_is_one_for_Icons() throws {
         let icon = Icon(path: "~/")
             .type(.fileicon)
             .path("i'm on a new path")
@@ -17,6 +17,25 @@ class FluentAPITests: XCTestCase {
         XCTAssertEqual(
             icon,
             try JSONHelper().iconObject(from: expectedOutput)
+        )
+    }
+
+    func test_that_there_is_one_for_Variables() {
+        let variable = Variable()
+            .name("color")
+            .value("blue")
+            .name("mood")
+
+        let expectedOutput = """
+        {
+            "name": "mood",
+            "value": "blue"
+        }
+        """
+
+        XCTAssertEqual(
+            variable,
+            try JSONHelper().variableObject(from: expectedOutput)
         )
     }
 }
