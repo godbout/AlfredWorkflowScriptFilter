a69429e93f392c17c16131db0d79d8c29266ef3f
Guill Lo <guill.bout@gmail.com>
2021-03-06 00:20:42 +0800

    fix name of the MoreBetterAPI file and class

--- Tests/AlfredWorkflowScriptFilterTests/MoreBetterAPITests.swift
+++ Tests/AlfredWorkflowScriptFilterTests/MoreBetterAPITests.swift
@@ -1,7 +1,7 @@
 @testable import AlfredWorkflowScriptFilter
 import XCTest
 
-class MoreBetterAPI: XCTestCase {
+class MoreBetterAPITests: XCTestCase {
     func test_that_there_is_a_shorthand_to_add_only_one_variable_to_an_item() throws {
         let item = Item(title: "the title")
             .variable(Variable(name: "breakfast", value: "whisky"))
@@ -21,7 +21,7 @@ class MoreBetterAPI: XCTestCase {
         )
     }
 
-    func test_that_there_is_a_shorthand_to_add_only_one_variable_to_a_mod() throws {
+    func test_that_there_is_a_shorthand_to_add_only_one_variable_to_a_modifier() throws {
         let cmd = Cmd().variable(Variable(name: "language", value: "yes"))
 
         let expectedOutput = """
@@ -38,7 +38,7 @@ class MoreBetterAPI: XCTestCase {
         )
     }
 
-    func test_that_there_is_a_shorthand_to_add_only_one_mod_to_an_item() throws {
+    func test_that_there_is_a_shorthand_to_add_only_one_modifier_to_an_item() throws {
         let item = Item(title: "i t'aime")
             .mod(Alt().subtitle("je te aime"))
 
