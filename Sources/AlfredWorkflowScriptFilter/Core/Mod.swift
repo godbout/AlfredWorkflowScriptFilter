f622a983446935dac69a076b315156e9ad155447
Guill Lo <guill.bout@gmail.com>
2021-03-14 19:33:28 +0800

    make abstract mod and concrete classes public

--- Sources/AlfredWorkflowScriptFilter/Core/Mod.swift
+++ Sources/AlfredWorkflowScriptFilter/Core/Mod.swift
@@ -1,17 +1,17 @@
 // TODO: make the Mod class abstract. Swift?
-class Mod: HasSubtitle, HasArg, HasIcon, HasVariables, HasValidity, Codable {
-    var subtitle: String?
-    var arg: String?
+public class Mod: HasSubtitle, HasArg, HasIcon, HasVariables, HasValidity, Codable {
+    public var subtitle: String?
+    public var arg: String?
     var valid: Bool?
     var icon: Icon?
-    var variables: [String: String]?
+    public var variables: [String: String]?
 
     // TODO: check if a subtitle in mandatory in Mod ScriptFilter
-    init() {}
+    public init() {}
 }
 
 extension Mod: Equatable {
-    static func == (lhs: Mod, rhs: Mod) -> Bool {
+    public static func == (lhs: Mod, rhs: Mod) -> Bool {
         lhs.subtitle == rhs.subtitle
             && lhs.arg == rhs.arg
             && lhs.valid == rhs.valid
@@ -20,8 +20,8 @@ extension Mod: Equatable {
     }
 }
 
-class Ctrl: Mod {}
-class Cmd: Mod {}
-class Alt: Mod {}
-class Fn: Mod {}
-class Shift: Mod {}
+public class Ctrl: Mod {}
+public class Cmd: Mod {}
+public class Alt: Mod {}
+public class Fn: Mod {}
+public class Shift: Mod {}
