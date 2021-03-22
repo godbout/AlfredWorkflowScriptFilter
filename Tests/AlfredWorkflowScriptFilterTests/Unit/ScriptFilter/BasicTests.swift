d1b3d08ea672bc18a70f935dfd5e822c0b448fff
Guill Lo <guill.bout@gmail.com>
2021-03-22 14:17:05 +0800

    refactor tests structure

--- Tests/AlfredWorkflowScriptFilterTests/Unit/ScriptFilter/BasicTests.swift
+++ Tests/AlfredWorkflowScriptFilterTests/Unit/ScriptFilter/BasicTests.swift
@@ -1,7 +1,7 @@
 @testable import AlfredWorkflowScriptFilter
 import XCTest
 
-final class ScriptFilterTests: XCTestCase {
+final class ScriptFilterBasicTests: XCTestCase {
     override func tearDown() {
         ScriptFilter.reset()
 
@@ -9,7 +9,7 @@ final class ScriptFilterTests: XCTestCase {
     }
 }
 
-extension ScriptFilterTests {
+extension ScriptFilterBasicTests {
     func test_that_it_may_contain_nothing() {
         XCTAssertEqual(
             ScriptFilter.output(),
@@ -285,137 +285,6 @@ extension ScriptFilterTests {
 
         XCTAssertEqual(
             try JSONHelper().scriptFilterObject(from: output),
-            try JSONHelper().scriptFilterObject(from: expectedOutput)
-        )
-    }
-
-    func test_that_it_can_filter_items_by_title() throws {
-        ScriptFilter.add(
-            Item(title: "Olá"),
-            Item(title: "Bonjour"),
-            Item(title: "Hello")
-        )
-
-        let expectedOutputNotFiltered = """
-        {
-            "items": [
-                {
-                    "title": "Olá"
-                },
-                {
-                    "title": "Bonjour"
-                },
-                {
-                    "title": "Hello"
-                }
-            ]
-        }
-        """
-
-        XCTAssertEqual(
-            try JSONHelper().scriptFilterObject(from: ScriptFilter.output()),
-            try JSONHelper().scriptFilterObject(from: expectedOutputNotFiltered)
-        )
-
-        ScriptFilter.filterItems(with: "Bon")
-        let expectedOutputFiltered = """
-        {
-            "items": [
-                {
-                    "title": "Bonjour"
-                }
-            ]
-        }
-        """
-
-        XCTAssertEqual(
-            try JSONHelper().scriptFilterObject(from: ScriptFilter.output()),
-            try JSONHelper().scriptFilterObject(from: expectedOutputFiltered)
-        )
-    }
-
-    func test_that_it_can_filter_items_by_subtitle() throws {
-        ScriptFilter.add(
-            Item(title: "Delete").subtitle("Select to delete timer"),
-            Item(title: "Choose").subtitle("Select to choose timer"),
-            Item(title: "See").subtitle("Select to see timer"),
-            Item(title: "Continue").subtitle("Select to continue timer")
-        )
-
-        let expectedOutput = """
-        {
-            "items": [
-                {
-                    "title": "See",
-                    "subtitle": "Select to see timer"
-                }
-            ]
-        }
-        """
-
-        ScriptFilter.filterItems(with: "see", in: .subtitle)
-
-        XCTAssertEqual(
-            try JSONHelper().scriptFilterObject(from: ScriptFilter.output()),
-            try JSONHelper().scriptFilterObject(from: expectedOutput)
-        )
-    }
-
-    func test_that_filtering_by_an_empty_string_does_not_filter_items() throws {
-        ScriptFilter.add(
-            Item(title: "bananas"),
-            Item(title: "apricots"),
-            Item(title: "tomatoes")
-        )
-
-        let expectedOutput = """
-        {
-            "items": [
-                {
-                    "title": "bananas"
-                },
-                {
-                    "title": "apricots"
-                },
-                {
-                    "title": "tomatoes"
-                }
-            ]
-        }
-        """
-
-        ScriptFilter.filterItems(with: "")
-
-        print("output")
-        print(ScriptFilter.output())
-
-        XCTAssertEqual(
-            try JSONHelper().scriptFilterObject(from: ScriptFilter.output()),
-            try JSONHelper().scriptFilterObject(from: expectedOutput)
-        )
-    }
-
-    func test_that_filtering_items_is_case_insensitive() throws {
-        ScriptFilter.add(
-            Item(title: "Renault"),
-            Item(title: "Peugeot"),
-            Item(title: "Citroën")
-        )
-
-        let expectedOutput = """
-        {
-            "items": [
-                {
-                    "title": "Peugeot"
-                }
-            ]
-        }
-        """
-
-        ScriptFilter.filterItems(with: "peu")
-
-        XCTAssertEqual(
-            try JSONHelper().scriptFilterObject(from: ScriptFilter.output()),
             try JSONHelper().scriptFilterObject(from: expectedOutput)
         )
     }
